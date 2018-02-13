169f1891a4b151befb75e151dc6ce2334f900217
Sara Mueller <muel5295@stthomas.edu>
2018-02-13 12:35:10 -0600

    I added the PreDeterminedRoll method due 2/17, not totally finished but its started!

---
+++ src/SkunkDie.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class SkunkDie
+{
+	//Test class
+	private ArrayList <Integer> die = new <Integer> ArrayList();
+	Scanner scan = new Scanner(System.in);
+	private int lastRoll;
+	
+	public SkunkDie()
+	{
+		die.add(1);
+		die.add(2);
+		die.add(3);
+		die.add(4);
+		die.add(5);
+		die.add(6);
+		lastRoll = 0;
+		
+		
+	}
+	
+	public void PreDeterminedRoll()
+	{
+		System.out.println("Would you like to roll the dice? Enter 1 for yes 2 for no");
+		int ans = scan.nextInt();
+		lastRoll = 0;
+		while (ans == 1)
+		{
+		System.out.println(die.get(lastRoll));
+		System.out.println("Would you like to roll again?");
+		ans = scan.nextInt();
+		if(ans ==1)
+		{
+		lastRoll++;
+		continue;
+		}
+		if (ans == 2)
+		{
+			break;
+		}
+		}
+		
+		if(ans == 2)
+		{
+			System.out.println("Thanks for playing!");
+		}
+			
+	}
+		
+		
+	
+	
+	
+	public static void main(String[] args)
+	{
+		SkunkDie s = new SkunkDie();
+		s.PreDeterminedRoll();
+	}
+	
+}
+
