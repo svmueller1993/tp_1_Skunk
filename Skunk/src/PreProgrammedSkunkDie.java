6acb8af1d3c87602279763703c35087b0e14b234
bagy2017 <solt2309@stthomas.edu>
2018-02-14 00:46:58 -0600

    Created SkunkDice, SkunkDie and PreProgrammedSkunkDie class following documents.

---
+++ Skunk/src/PreProgrammedSkunkDie.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+/**
+ * This die class returns predefined values on rolling.
+ *
+ */
+public class PreProgrammedSkunkDie extends SkunkDie
+{
+	// Test class
+	private ArrayList<Integer> predefinedValues = new ArrayList<Integer>();
+	private int predefinedRoll;
+	
+	public PreProgrammedSkunkDie()
+	{
+		predefinedValues.add(1);
+		predefinedValues.add(2);
+		predefinedValues.add(3);
+		predefinedValues.add(4);
+		predefinedValues.add(5);
+		predefinedValues.add(6);
+		predefinedRoll = 0;
+	}
+	
+	@Override
+	public void roll()
+	{	
+		predefinedRoll++;
+	}
+	
+	@Override
+	public int getLastRoll()
+	{
+		if (predefinedRoll == predefinedValues.size()) {
+			predefinedRoll = 0;
+		}
+		return predefinedValues.get(predefinedRoll);
+	}
+}
--- Skunk/src/SkunkApp.java
+++ Skunk/src/SkunkApp.java
@@ -1,5 +1,45 @@
-
+/**
+ * This class starts Skunk game.
+ * 
+ * @author Bagy
+ *
+ */
 public class SkunkApp
 {
+	public static void main(String[] args)
+	{
+		SkunkApp app = new SkunkApp();
+		app.PreDeterminedRoll();
+	}
+
+	/**
+	 * Testing roll method for pre-determined outcomes of the die roll
+	 */
+	public void PreDeterminedRoll()
+	{
+		StdOut.println("Would you like to roll the dice? Enter 1 for yes 2 for no");
+		int ans = StdIn.readInt();
+
+		PreProgrammedSkunkDie die1 = new PreProgrammedSkunkDie();
+		PreProgrammedSkunkDie die2 = new PreProgrammedSkunkDie();
+		SkunkDice dice = new SkunkDice(die1, die2);
+		while (ans == 1)
+		{
+			System.out.println(dice.getLastRoll1());
+			System.out.println("Would you like to roll again?");
+			ans = StdIn.readInt();
+			if (ans == 1)
+			{
+				dice.roll();
+				continue;
+			}
+			if (ans == 2)
+			{
+				StdOut.println("Thanks for playing!");
+				break;
+			}
+		}
+
+	}
 
 }
